a32a8fd44de16457418c49732a7aa6d5062f1162
Jason <28811675+xynoipse@users.noreply.github.com>
2020-10-02 22:52:46 +0800

    test(returns-api): return already processed

--- tests/integration/routes/returns.test.js
+++ tests/integration/routes/returns.test.js
@@ -76,4 +76,13 @@ describe('/api/returns', () => {
 
     expect(res.status).toBe(404);
   });
+
+  it('should return 400 if return is already processed', async () => {
+    rental.dateReturned = new Date();
+    await rental.save();
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+  });
 });
